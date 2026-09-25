6a7c4ba7130e30c1753a912ba300c6971ea7d918
AlexPranschke <alexpranschke@gmail.com>
2026-09-25 15:14:50 +0200

    movie

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,32 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
 
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("The Matrix 2");
+        movie.setDescription("Reality is a bigger simulation");
+
+        // test add()
+        Movie savedMovie = movieService.add(movie);
+        System.out.println("Saved movie: "
+                + savedMovie.getTitle()
+                + " - "
+                + savedMovie.getDescription());
+
+        // test get()
+        Movie movieFromDb = movieService.get(savedMovie.getId());
+        System.out.println("Movie from DB: "
+                + movieFromDb.getTitle()
+                + " - "
+                + movieFromDb.getDescription());
     }
 }
