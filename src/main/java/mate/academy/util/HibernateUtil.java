f90ddfe6e474552436ed440c83d29b05769e83f3
AlexPranschke <alexpranschke@gmail.com>
2026-09-25 17:20:36 +0200

    bs version stuff

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -3,7 +3,12 @@ package mate.academy.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateUtil {
+//for security reasons, the password is stored in an
+// environment variable and not in the hibernate.cfg.xml file
+/*public class HibernateUtil {
+
+    private HibernateUtil() {
+    }
 
     private static final SessionFactory instance = initSessionFactory();
 
@@ -17,4 +22,23 @@ public class HibernateUtil {
         return new Configuration().configure()
                 .setProperty("hibernate.connection.password", password).buildSessionFactory();
     }
+}*/
+public class HibernateUtil {
+
+    private static final SessionFactory instance = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return instance;
+    }
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
 }
+
+
